db8a83da9a1ba0f90b83a4a5f4a37d369d11663d
imyaxir <49075250+imyaxir@users.noreply.github.com>
2019-03-30 00:16:12 +0500

    new project created

--- app/src/main/java/com/example/jobportal/MainActivity.java
+++ app/src/main/java/com/example/jobportal/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.jobportal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText e;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
